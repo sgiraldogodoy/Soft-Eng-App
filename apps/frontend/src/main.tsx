41b69e4871a3f415c56c4c54ee45855acf3027eb
Matthew Franco <matthew.m.franco@gmail.com>
2024-04-01 18:24:18 -0400

    feat: added auth0

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/globals.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-x61j30sgxmn7t3u3.us.auth0.com"
+    clientId="a07mB0uQsSJDFjtRqBX7nNAzbDkWmUY5"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
 );
