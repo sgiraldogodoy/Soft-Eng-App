6cf21addd62d7360b0545c448557a285d4b9cf88
Matthew Franco <matthew.m.franco@gmail.com>
2024-04-01 14:45:24 -0400

    feat: added BWH logo, hero section image, and styles to components

---
+++ apps/frontend/src/components/HeroSection.tsx
@@ -0,0 +1,21 @@
+export default function HeroSection() {
+  return (
+    <div className="flex-col justify-center align-middle">
+      <div className="flex gap-2 justify-center items-center">
+        <img src="public/BWH%20Logo.svg" alt="BWH Logo" className="relative" />
+        <h1 className="text-left">
+          Welcome to <br />{" "}
+          <span className="text-theme-blue font-black">
+            Brigham and Women's Hospital
+          </span>
+        </h1>
+      </div>
+      <img
+        src="../../public/bwh-corridor.jpeg"
+        alt="BWH Corridor"
+        height="1202"
+        width="390"
+      />
+    </div>
+  );
+}
