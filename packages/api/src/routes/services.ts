068234492b7ddab0496693cb150823f7206ffc38
sgiraldogodoy <sgiraldogodoy@wpi.edu>
2024-04-02 19:44:59 -0400

    fix: rolled back extra changes and made id optional for flower request trpc

--- packages/api/src/routes/services.ts
+++ packages/api/src/routes/services.ts
@@ -8,6 +8,7 @@ export const serviceRequestRouter = router({
   createFlowerRequest: publicProcedure
     .input(
       z.object({
+        id: z.string().optional(),
         nodeId: z.string(),
         flowerName: z.string(),
         requestDate: z.date().optional(),
