d92a9fc381cfb8bcd21a1a2dc560c89ae4913e70
Ace Beattie <acebeattie@gmail.com>
2024-04-02 19:36:54 -0400

    feat: yolo again

--- apps/frontend/src/routes/PathFind.tsx
+++ apps/frontend/src/routes/PathFind.tsx
@@ -40,7 +40,7 @@ export default function PathFind() {
   };
 
   return (
-    <div className="inputs">
+    <div className="max-h-screen overflow-hidden">
       <h1 className="mt-4 text-2xl flex items-center justify-center font-bold mb-4 w-5/6">
         Where would you like to go?
       </h1>
