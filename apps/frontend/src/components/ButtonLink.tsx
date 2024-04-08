823129b356d19d439b98509351fffebcc194b899
Ace Beattie <acebeattie@gmail.com>
2024-04-08 18:28:55 -0400

    feat: add hover state to matched route on dashboard

--- apps/frontend/src/components/ButtonLink.tsx
+++ apps/frontend/src/components/ButtonLink.tsx
@@ -1,7 +1,8 @@
-import { Link } from "wouter";
+import { Link, useRoute } from "wouter";
 import { Button } from "./ui/button";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 import { LucideIcon } from "lucide-react";
+import clsx from "clsx";
 
 interface Props {
   link: string;
@@ -10,10 +11,17 @@ interface Props {
 }
 
 export function ButtonLink({ link, name, icon: Icon }: Props) {
+  const [match] = useRoute(link);
+
   return (
     <Tooltip>
       <TooltipTrigger>
-        <Button asChild variant="ghost" size="icon">
+        <Button
+          className={clsx(match && "bg-slate-100 text-slate-900")}
+          asChild
+          variant="ghost"
+          size="icon"
+        >
           <Link to={link}>
             <Icon size={28} />
           </Link>
