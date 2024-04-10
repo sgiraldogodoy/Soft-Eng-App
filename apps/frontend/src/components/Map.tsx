00353439840f0a78f6b8707359e2aa17023fca14
ColeGW <68122848+ColeGW@users.noreply.github.com>
2024-04-09 20:22:37 -0400

    fix: routing issue

--- apps/frontend/src/components/Map.tsx
+++ apps/frontend/src/components/Map.tsx
@@ -153,7 +153,7 @@ export default function Map({
 
   return (
     <div
-      className="relative"
+      className="relative h-full"
       ref={containerRef}
       style={{
         position: "relative",
