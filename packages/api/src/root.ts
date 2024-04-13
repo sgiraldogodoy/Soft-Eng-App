5db3bc4d7bde099d17a79cfdcb19dcaa9dbc84c6
Justin Y <156143330+jyip168@users.noreply.github.com>
2024-04-13 17:16:11 -0400

    feat: implemented the tRPC route for the AV Request on the api

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,10 +1,12 @@
 import { serviceRequestRouter } from "./routes/services.ts";
+import { avRequestRouter } from "./routes/Services/avRouter.ts";
 import { router } from "./trpc";
 import { dbRouter } from "./routes/db-router.ts";
 import { Node } from "./routes/node.ts";
 
 export const appRouter = router({
   service: serviceRequestRouter,
+  av: avRequestRouter,
   db: dbRouter,
   node: Node,
 });
