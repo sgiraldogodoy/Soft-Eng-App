c96d473e7144073835b2edafbd52f99067f355ce
Ace Beattie <acebeattie@gmail.com>
2024-04-14 19:59:19 -0400

    feat: add protected routes to trpc

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,9 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/globals.css";
 import App from "./App.tsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-x61j30sgxmn7t3u3.us.auth0.com"
+      clientId="a07mB0uQsSJDFjtRqBX7nNAzbDkWmUY5"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: "https://dev-x61j30sgxmn7t3u3.us.auth0.com/api/v2/",
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
 );
