45cb2cef32024648b171536cfb74d89ce1d1ab1e
ColeGW <68122848+ColeGW@users.noreply.github.com>
2024-04-14 20:50:22 -0400

    feat: secruity router db

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -2,9 +2,11 @@ import { serviceRequestRouter } from "./routes/services.ts";
 import { router } from "./trpc";
 import { dbRouter } from "./routes/db-router.ts";
 import { Node } from "./routes/node.ts";
+import { SecurityRouter } from "./routes/Services/securityRouter.ts";
 
 export const appRouter = router({
   service: serviceRequestRouter,
+  security: SecurityRouter,
   db: dbRouter,
   node: Node,
 });
--- packages/api/src/routes/Services/securityRouter.ts
+++ packages/api/src/routes/Services/securityRouter.ts
@@ -1,7 +1,86 @@
-/*
 import { publicProcedure } from "../../trpc";
 import { router } from "../../trpc";
 import { z } from "zod";
+import { transformCreateServiceInput } from "../../../utils/serviceInputTransformer.ts";
+import { baseService, security } from "common";
 
-export const SecurityRouter = router({});
-*/
+export const SecurityRouter = router({
+  //Security Request Service
+  createOne: publicProcedure
+    .input(
+      baseService
+        .extend({ data: security })
+        .extend({ type: z.literal("SECURITY").default("SECURITY") })
+        .transform(transformCreateServiceInput),
+    )
+    .mutation(async ({ input, ctx }) => {
+      console.log(input);
+      return await ctx.db.security.create({
+        data: input,
+      });
+    }),
+  getOne: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      }),
+    )
+    .query(async ({ input, ctx }) => {
+      // get a security request
+
+      return ctx.db.security.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    // get all security requests
+    return ctx.db.security.findMany({
+      include: {
+        service: true,
+      },
+    });
+  }),
+  deleteOne: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      // delete a security request
+      await ctx.db.security.delete({
+        where: {
+          id: input.id,
+        },
+      });
+
+      return { message: "Security request deleted" };
+    }),
+
+  deleteAll: publicProcedure.mutation(async ({ ctx }) => {
+    // delete all security requests
+    await ctx.db.security.deleteMany({});
+
+    return { message: "All security requests deleted" };
+  }),
+  deleteMany: publicProcedure
+    .input(
+      z.object({
+        ids: z.array(z.string()),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      // delete multiple security requests
+      await ctx.db.security.deleteMany({
+        where: {
+          id: {
+            in: input.ids,
+          },
+        },
+      });
+
+      return { message: "Security requests deleted" };
+    }),
+});
