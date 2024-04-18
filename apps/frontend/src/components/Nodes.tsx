327343e6b1fabf89f46a1f8bdcf3f9e02a3460fc
ColeGW <68122848+ColeGW@users.noreply.github.com>
2024-04-18 11:29:13 -0400

    Update Nodes.tsx
    got rid of comments

--- apps/frontend/src/components/Nodes.tsx
+++ apps/frontend/src/components/Nodes.tsx
@@ -6,8 +6,6 @@ import {
 } from "../utils/scaleCoordinate.ts";
 import { trpc } from "@/utils/trpc.ts";
 import { useSelectNodes } from "@/components/createNode.tsx";
-// import { NewNodeDialog } from "@/components/newNodeDialog.tsx";
-// import { createNode } from "@/components/createNode.tsx";
 
 const origImageWidth = 5000;
 const origImageHeight = 3400;
@@ -47,59 +45,14 @@ export function Nodes({
   const [hoveredNodeString, setHoveredNodeString] = useState<string | null>(
     null,
   );
-  // const [openDialog, setOpenDialog] = useState(false);
-  // const [newNodeX, setNewNodeX] = useState(0);
-  // const [newNodeY, setNewNodeY] = useState(0);
 
   const utils = trpc.useUtils();
   const nodeUpdate = trpc.node.updateOne.useMutation();
   const deleteNode = trpc.node.deleteOne.useMutation();
   const createEdge = trpc.edge.createOne.useMutation();
-  // const createNode = trpc.node.createOne.useMutation();
 
   const { firstNode, setNode, clearNodes } = useSelectNodes();
-  // const handleCreateNodeSubmit = (data: Node) => {
-  //   createNode.mutate(
-  //     {
-  //       data,
-  //     },
-  //     {
-  //       onSuccess: () => {
-  //         utils.node.getAll.invalidate();
-  //       },
-  //     },
-  //   );
-  //   setOpenDialog(false);
-  // };
-  // const handleCreateNode = (
-  //   e: React.MouseEvent<HTMLDivElement, MouseEvent>,
-  // ) => {
-  //   const target = e.currentTarget;
-  //   target.style.position = "absolute";
-  //   const offsetX = e.clientX - parseFloat(target.style.left || "0");
-  //   const offsetY = e.clientY - parseFloat(target.style.top || "0");
-  //   const newNodeX = reverseScaleCoordinate(
-  //     e.clientX - offsetX,
-  //     imgWidth,
-  //     origImageWidth,
-  //     0,
-  //     dragOffset.x,
-  //     scale,
-  //   );
-  //   const newNodeY = reverseScaleCoordinate(
-  //     e.clientY - offsetY,
-  //     imgHeight,
-  //     origImageHeight,
-  //     0,
-  //     dragOffset.y,
-  //     scale,
-  //   );
-  //
-  //   setNewNodeX(Math.floor(newNodeX));
-  //   setNewNodeY(Math.floor(newNodeY));
-  //   setOpenDialog(true);
-  // };
-
+  
   const handleCreateEdge = () => {
     if (!hoveredNode) return;
     createEdge.mutate(
@@ -131,7 +84,7 @@ export function Nodes({
           },
         },
       );
-      // }
+     
     }
   };
 
@@ -314,14 +267,6 @@ export function Nodes({
           Node ID: {hoveredNode}
         </div>
       )}
-      {/*{openDialog && (*/}
-      {/*  <NewNodeDialog*/}
-      {/*    open={openDialog}*/}
-      {/*    x={newNodeX}*/}
-      {/*    y={newNodeY}*/}
-      {/*    onSubmit={handleCreateNodeSubmit}*/}
-      {/*  />*/}
-      {/*)}*/}
     </div>
   );
 }
