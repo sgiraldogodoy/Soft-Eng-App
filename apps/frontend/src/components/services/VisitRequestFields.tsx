c65d8580c420a9b29d810cd0c8383a13c2bb5ab5
Justin <jyip168@gmail.com>
2024-04-18 21:40:06 -0400

    feat: added fields for visit requests

---
+++ apps/frontend/src/components/services/VisitRequestFields.tsx
@@ -0,0 +1,57 @@
+import { z } from "zod";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+
+import { Input } from "@/components/ui/input.tsx";
+
+export const VisitRequestSchema = z.object({
+  type: z.literal("visit-request"),
+  visitorName: z.string(),
+  visiteeName: z.string(),
+});
+
+const VisitRequestFields = () => {
+  return (
+    <>
+      <div className="flex gap-2 items-center flex-1">
+        <FormField
+          name="data.visitorName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Visitor Name</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormDescription>Who is visiting?</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="data.patientName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Patient Name</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormDescription>Who is being visited?</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </div>
+      <div>
+        <p className="text-xs"> created by Justin Yip</p>
+      </div>
+    </>
+  );
+};
+
+export default VisitRequestFields;
