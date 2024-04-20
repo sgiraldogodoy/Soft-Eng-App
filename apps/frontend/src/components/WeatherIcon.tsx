dcc9108b37e3c922d7282b1baabca94cd452855b
Matthew Franco <matthew.m.franco@gmail.com>
2024-04-20 17:46:19 -0400

    fix: changed the color of all weather icons

--- apps/frontend/src/components/WeatherIcon.tsx
+++ apps/frontend/src/components/WeatherIcon.tsx
@@ -23,58 +23,58 @@ export default function WeatherIcon({ iconCode }: WeatherIconProps) {
   const getIconByCode = () => {
     switch (iconCode) {
       case "01d": {
-        return <Sun size={iconSize} />;
+        return <Sun size={iconSize} color="#ffffff" />;
       }
       case "01n": {
-        return <Moon size={iconSize} />;
+        return <Moon size={iconSize} color="#ffffff" />;
       }
       case "02d": {
-        return <CloudSun size={iconSize} />;
+        return <CloudSun size={iconSize} color="#ffffff" />;
       }
       case "02n": {
-        return <CloudMoon size={iconSize} />;
+        return <CloudMoon size={iconSize} color="#ffffff" />;
       }
       case "03d": {
-        return <Cloud size={iconSize} />;
+        return <Cloud size={iconSize} color="#ffffff" />;
       }
       case "03n": {
-        return <Cloud size={iconSize} />;
+        return <Cloud size={iconSize} color="#ffffff" />;
       }
       case "04d": {
-        return <Cloudy size={iconSize} />;
+        return <Cloudy size={iconSize} color="#ffffff" />;
       }
       case "04n": {
         return <Cloudy size={iconSize} color="#FFFFFF" />;
       }
       case "09d": {
-        return <CloudRain size={iconSize} />;
+        return <CloudRain size={iconSize} color="#ffffff" />;
       }
       case "09n": {
-        return <CloudRain size={iconSize} />;
+        return <CloudRain size={iconSize} color="#ffffff" />;
       }
       case "10d": {
-        return <CloudSunRain size={iconSize} />;
+        return <CloudSunRain size={iconSize} color="#ffffff" />;
       }
       case "10n": {
-        return <CloudMoonRain size={iconSize} />;
+        return <CloudMoonRain size={iconSize} color="#ffffff" />;
       }
       case "11d": {
-        return <CloudLightning size={iconSize} />;
+        return <CloudLightning size={iconSize} color="#ffffff" />;
       }
       case "11n": {
-        return <CloudLightning size={iconSize} />;
+        return <CloudLightning size={iconSize} color="#ffffff" />;
       }
       case "13d": {
-        return <Snowflake size={iconSize} />;
+        return <Snowflake size={iconSize} color="#ffffff" />;
       }
       case "13n": {
-        return <Snowflake size={iconSize} />;
+        return <Snowflake size={iconSize} color="#ffffff" />;
       }
       case "50d": {
-        return <CloudFog size={iconSize} />;
+        return <CloudFog size={iconSize} color="#ffffff" />;
       }
       case "50n": {
-        return <CloudFog size={iconSize} />;
+        return <CloudFog size={iconSize} color="#ffffff" />;
       }
     }
   };
