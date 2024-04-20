c75ba6caf31cb5ec49079c77e42756359e406f22
Justin <jyip168@gmail.com>
2024-04-20 16:25:06 -0400

    feat: started on the about page

---
+++ apps/frontend/src/routes/AboutPage.tsx
@@ -0,0 +1,27 @@
+export default function AboutPage() {
+  return (
+    <div className="h-screen flex justify-center content-center py-52">
+      <div>
+        <h1></h1>
+      </div>
+      <div className="wong & ian"></div>
+      <div className="team"></div>
+      <div className=" thank hospital and rep"></div>
+      <div>
+        <footer className="disclaimer"></footer>
+      </div>
+    </div>
+  );
+}
+
+/*
+    Requirements:
+
+    The About page lists the following: WPI
+Computer Science Department, CS3733-D24 Software Engineering, Prof. Wilson Wong,
+the name of your team coach, and the names, positions, and photos of each team
+member. Also thank Brigham and Women’s Hospital and their representative, Andrew
+Shinn. Include at the bottom, “The Brigham & Women’s Hospital maps and data used in
+this application are copyrighted and provided for the sole use of educational purposes.”
+
+ */
