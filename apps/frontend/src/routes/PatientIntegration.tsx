e32aadd7e3ae00ed420eb2f65a0899487f27e033
Daniel Reynolds <djreynolds1@wpi.edu>
2024-04-20 18:37:27 -0400

    feat: created dummy patient retreival element to (when implemented w/ backend)

--- apps/frontend/src/routes/PatientIntegration.tsx
+++ apps/frontend/src/routes/PatientIntegration.tsx
@@ -5,13 +5,14 @@ import {
   CardContent,
 } from "@/components/ui/card.tsx";
 import PatientCreationForm from "@/components/services/PatientCreationForm.tsx";
+import PatientRetrevial from "@/components/services/PatientRetrevialForm.tsx";
 
 export default function PatientIntegration() {
   return (
     <div className="h-full w-full flex flex-row gap-2 items-center justify-center px-5 py-3">
       <Card className="h-full bg-white/90 drop-shadow-md shadow-inner backdrop-blur-md flex-1 flex flex-col overflow-auto">
         <CardHeader>
-          <CardTitle className="capitalize">Patient Creation</CardTitle>
+          <CardTitle>Patient Creation</CardTitle>
         </CardHeader>
         <CardContent className="flex-1 flex flex-col mt-5 gap-2">
           <PatientCreationForm />
@@ -19,10 +20,10 @@ export default function PatientIntegration() {
       </Card>
       <Card className="h-full bg-white/90 drop-shadow-md shadow-inner backdrop-blur-md flex-1 flex flex-col overflow-auto">
         <CardHeader>
-          <CardTitle className="capitalize">Patient Lookup</CardTitle>
+          <CardTitle>Patient Lookup</CardTitle>
         </CardHeader>
-        <CardContent className="flex-1 flex flex-col justify-center items-center gap-2">
-          <p>This is the patient integration page 2.</p>
+        <CardContent className="flex-1 flex flex-col gap-2">
+          <PatientRetrevial />
         </CardContent>
       </Card>
     </div>
