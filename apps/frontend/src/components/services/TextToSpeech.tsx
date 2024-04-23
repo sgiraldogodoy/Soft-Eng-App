9df778e53d4d98f7e82c281b4abd570607deb3f3
Daniel Reynolds <djreynolds1@wpi.edu>
2024-04-22 22:25:04 -0400

    feat: added new text to speech, based on new API that allows for a lot more calls

---
+++ apps/frontend/src/components/services/TextToSpeech.tsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import { Play, Pause, StopCircle } from "lucide-react";
+
+const TextToSpeech = ({ text }: { text: string }) => {
+  const [isPaused, setIsPaused] = useState(false);
+  const [utterance, setUtterance] = useState<SpeechSynthesisUtterance | null>(
+    null,
+  );
+
+  useEffect(() => {
+    const synth = window.speechSynthesis;
+    const u = new SpeechSynthesisUtterance(text);
+
+    setUtterance(u);
+
+    return () => {
+      synth.cancel();
+    };
+  }, [text]);
+
+  const handlePlay = () => {
+    const synth = window.speechSynthesis;
+
+    if (isPaused) {
+      synth.resume();
+    }
+
+    if (!utterance) {
+      return;
+    }
+
+    synth.speak(utterance);
+
+    setIsPaused(false);
+  };
+
+  const handlePause = () => {
+    const synth = window.speechSynthesis;
+
+    synth.pause();
+
+    setIsPaused(true);
+  };
+
+  const handleStop = () => {
+    const synth = window.speechSynthesis;
+
+    synth.cancel();
+
+    setIsPaused(false);
+  };
+
+  return (
+    <div className="flex flex-row justify-items justify-center py-2 space-x-3">
+      <button onClick={handlePlay}>
+        <Play />
+      </button>
+      <button onClick={handlePause}>
+        <Pause />
+      </button>
+      <button onClick={handleStop}>
+        <StopCircle />
+      </button>
+    </div>
+  );
+};
+
+export default TextToSpeech;
