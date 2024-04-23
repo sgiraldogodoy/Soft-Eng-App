744ae747b54c194d4d1578cb75b9fff06e5a5b77
Keenan-Porter <116773589+Keenan-Porter@users.noreply.github.com>
2024-04-23 15:48:10 -0400

    feat: created new icons

--- apps/frontend/src/components/MapKeyAccordion.tsx
+++ apps/frontend/src/components/MapKeyAccordion.tsx
@@ -11,7 +11,7 @@ export default function MapKeyAccordion() {
       <Accordion
         type="single"
         collapsible
-        className="rounded-[10px] bg-white drop-shadow-lg absolute bottom-7 right-72"
+        className="rounded-[8px] bg-white drop-shadow-lg absolute bottom-7 right-72"
       >
         <AccordionItem value="Map">
           <AccordionTrigger className="font-bold mx-16">
@@ -21,61 +21,61 @@ export default function MapKeyAccordion() {
             <img
               src="./mapkeyicons/restroom.png"
               alt="restroom"
-              className="object-scale-down h-7 w-7 ps-2"
+              className="object-scale-down h-8 w-8 ps-2"
             />
             <p className="pe-5">Restrooms</p>
             <img
               src="./mapkeyicons/elevator.png"
               alt="elevator"
-              className="object-scale-down h-7 w-7 ps-2"
+              className="object-scale-down h-8 w-8 ps-2"
             />
             <p className="">Elevator</p>
             <img
               src="./mapkeyicons/ATM.png"
               alt="ATM"
-              className="object-scale-down h-7 w-7 ps-2"
+              className="object-scale-down h-8 w-8 ps-2"
             />
             <p className="">ATM</p>
             <img
               src="./mapkeyicons/cafe.png"
               alt="cafe"
-              className="object-scale-down h-7 w-7 ps-2"
+              className="object-scale-down h-8 w-8 ps-2"
             />
             <p className="">Cafe</p>
             <img
               src="./mapkeyicons/emergency.png"
               alt="emergency"
-              className="object-scale-down h-7 w-7 ps-2"
+              className="object-scale-down h-8 w-8 ps-2"
             />
             <p className="">Emergency</p>
             <img
               src="./mapkeyicons/giftshop.png"
               alt="giftshop"
-              className="object-scale-down h-7 w-7 ps-2"
+              className="object-scale-down h-8 w-8 ps-2"
             />
             <p className="">Gift Shop</p>
             <img
               src="./mapkeyicons/information.png"
               alt="information"
-              className="object-scale-down h-7 w-7 ps-2"
+              className="object-scale-down h-8 w-8 ps-2"
             />
             <p className="">Information</p>
             <img
               src="./mapkeyicons/parking.png"
               alt="parking"
-              className="object-scale-down h-7 w-7 ps-2"
+              className="object-scale-down h-8 w-8 ps-2"
             />
             <p className="">Parking</p>
             <img
               src="./mapkeyicons/pharmacy.png"
               alt="pharmacy"
-              className="object-scale-down h-7 w-7 ps-2"
+              className="object-scale-down h-8 w-8 ps-2"
             />
             <p className="">Pharmacy</p>
             <img
               src="./mapkeyicons/valet.png"
               alt="valet"
-              className="object-scale-down h-7 w-7 ps-2"
+              className="object-scale-down h-8 w-8 ps-2"
             />
             <p className="">Valet</p>
           </AccordionContent>
