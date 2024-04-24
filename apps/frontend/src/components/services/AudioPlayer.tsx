eb0554835a31817bbc84a95df01619d1206e15dd
Daniel Reynolds <djreynolds1@wpi.edu>
2024-04-23 22:07:37 -0400

    feat: finished creation of audio player with currently playing track and full album

--- apps/frontend/src/components/services/AudioPlayer.tsx
+++ apps/frontend/src/components/services/AudioPlayer.tsx
@@ -1,18 +1,28 @@
 import React, { useState, useEffect, useRef } from "react";
 import { SkipForward, SkipBack } from "lucide-react";
+import Dance from "/dancar.gif";
+
+type Song = {
+  url: string;
+  name: string;
+};
 
 type AudioPlayerProps = {
-  playlist: string[];
+  playlist: Song[];
 };
 
 function AudioPlayer({ playlist }: AudioPlayerProps) {
   const [currentSongIndex, setCurrentSongIndex] = useState<number>(0);
+  const [currentSongName, setCurrentSongName] = useState<string>(
+    playlist[0].name,
+  );
   const audioRef = useRef<HTMLAudioElement | null>(null);
 
   useEffect(() => {
     if (audioRef.current) {
-      audioRef.current.src = playlist[currentSongIndex];
+      audioRef.current.src = playlist[currentSongIndex].url;
       audioRef.current.play();
+      setCurrentSongName(playlist[currentSongIndex].name);
     }
   }, [currentSongIndex, playlist]);
 
@@ -27,7 +37,9 @@ function AudioPlayer({ playlist }: AudioPlayerProps) {
   };
 
   return (
-    <div className="bg-slate-400/80 flex flex-col text-center">
+    <div className="bg-gray-400/80 flex flex-col items-center rounded-2xl">
+      <p>Currently playing: {currentSongName}</p>
+      <img src={Dance} alt="Dancing" />
       <audio ref={audioRef} controls className="mt-32" />
       <div className="space-x-20 mt-5">
         <button onClick={handlePrev}>
