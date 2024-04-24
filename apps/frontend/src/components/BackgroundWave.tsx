49e26d662e7749816ac25c2f01e3311eb882d5c7
Ace Beattie <acebeattie@gmail.com>
2024-04-23 20:57:20 -0400

    fix: styles

---
+++ apps/frontend/src/components/BackgroundWave.tsx
@@ -0,0 +1,5 @@
+export default function BackgroundWave() {
+  return (
+    <img src="/wave.svg" alt="Wave" className="z-0 absolute bottom-0 left-0" />
+  );
+}
--- apps/frontend/src/components/layouts/DashboardLayout.tsx
+++ apps/frontend/src/components/layouts/DashboardLayout.tsx
@@ -129,7 +129,7 @@ export default function DashboardLayout({ children }: React.PropsWithChildren) {
           </nav>
         </aside>
 
-        <div className="flex-1 w-full overflow-auto relative bg-muted/40">
+        <div className="z-10 flex-1 w-full overflow-auto relative bg-muted/40">
           {children}
         </div>
       </div>
