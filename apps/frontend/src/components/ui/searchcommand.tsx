a16c20cfc2c6550f7ab5a74e34fd69b730328ba9
Keenan-Porter <116773589+Keenan-Porter@users.noreply.github.com>
2024-04-27 13:36:59 -0400

    feat: created searchcommand.tsx and got the skeleton running

---
+++ apps/frontend/src/components/ui/searchcommand.tsx
@@ -0,0 +1,61 @@
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+import { Button } from "@/components/ui/button.tsx";
+import React from "react";
+
+export default function SearchCommand() {
+  const [open, setOpen] = React.useState(false);
+
+  const buttonchange = () => {
+    setOpen((open) => !open);
+  };
+
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  return (
+    <>
+      <Button variant="ghost" onClick={buttonchange}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="lucide lucide-terminal"
+        >
+          <polyline points="4 17 10 11 4 5" />
+          <line x1="12" x2="20" y1="19" y2="19" />
+        </svg>
+      </Button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+}
