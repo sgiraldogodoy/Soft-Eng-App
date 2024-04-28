c2aa086e7ed7b600f563166246e47465a3a42173
KevinMcCrudden <lilkevinmccrudden@gmail.com>
2024-04-28 14:55:19 -0400

    feat: added libraries used

--- apps/frontend/src/routes/CreditPage.tsx
+++ apps/frontend/src/routes/CreditPage.tsx
@@ -242,7 +242,37 @@ const testimonialsLibraries = [
   {
     name: "Libraries Used",
     description:
-      "@auth0/auth0-react: ^2.1.4, @hookform/resolvers: ^3.3.4, @popperjs/core: ^2.11.8, @radix-ui/react-accordion: ^1.1.2, @radix-ui/react-alert-dialog: ^1.0.5, @radix-ui/react-checkbox: ^1.0.4, @radix-ui/react-dialog: ^1.0.5, @radix-ui/react-dropdown-menu: ^2.0.6, @radix-ui/react-icons: ^1.3.0, @radix-ui/react-label: ^2.0.2, @radix-ui/react-navigation-menu: ^1.1.4, @radix-ui/react-popover: ^1.0.7, @radix-ui/react-select: ^2.0.0, @radix-ui/react-slot: ^1.0.2, @radix-ui/react-switch: ^1.0.3, @radix-ui/react-tabs: ^1.0.4, @radix-ui/react-toast: ^1.1.5, @radix-ui/react-toggle: ^1.0.3, @radix-ui/react-toggle-group: ^1.0.4, @radix-ui/react-tooltip: ^1.0.7, @tanstack/react-query: ^5.28.9, @tanstack/react-table: ^8.9.3, @trpc/client: 11.0.0-next.327, @trpc/react-query: 11.0.0-next.327, @trpc/server: 11.0.0-next.327, accordion: ^3.0.2, add: ^2.0.6, api: workspace:^, autoprefixer: ^10.4.18, axios: ^1.4.0, class-variance-authority: ^0.7.0, clsx: ^2.1.0, cmdk: 0.2.0, common: workspace:^, date-fns: ^3.6.0, dropdown-menu: ^0.1.1, embla-carousel-react: ^8.0.0, framer-motion: ^11.0.28, localforage: ^1.10.0, lucide-react: ^0.363.0, luxon: ^3.4.4, match-sorter: ^6.3.1, next-themes: ^0.3.0, postcss: ^8.4.35, react: ^18.2.0, react-datetime: ^3.2.0, react-day-picker: ^8.10.1, react-dom: ^18.2.0, react-hook-form: ^7.51.3, react-hot-toast: ^2.4.1, react-qr-code: ^2.0.12, react-qrcode-logo: ^2.10.0, react-router-bootstrap: ^0.26.2, react-zoom-pan-pinch: ^3.1.0, serve: ^14.2.0, shadcn-ui: ^0.8.0, simplex-noise: ^4.0.1, sonner: ^1.4.41, sort-by: ^1.2.0, superjson: 1.13.3, tailwind-merge: ^2.2.2, tailwindcss: ^3.4.1, tailwindcss-animate: ^1.0.7, toggle-group: ^1.0.5, use-mask-input: ^3.3.7, wouter: ^3.1.0, zod: ^3.23.4, @types/luxon: ^3, @types/node: ^20.2.5, @types/prop-types: ^15.7.5, @types/react: ^18.0.37, @types/react-dom: ^18.0.11, @types/react-router-bootstrap: ^0.26.0, @vitejs/plugin-react-swc: ^3.0.0, database: workspace:*, eslint: latest, eslint-config-custom: workspace:*, eslint-plugin-react-hooks: ^4.6.0, eslint-plugin-react-refresh: ^0.4.1, prettier: latest, prettier-config-custom: workspace:*, sass: ^1.62.1, tailwindcss-dotted-background: ^1.1.0, tsconfig-custom: workspace:*, typescript: ^5.0.2, vite: ^4.3.9, vite-plugin-eslint: ^1.8.1, vitest: ^0.34.1, @types/express: ^4.17.21, @types/jsonwebtoken: ^9, @types/priorityqueuejs: ^1.0.4, express: ^4.19.2, jsonwebtoken: ^9.0.2, jwks-rsa: ^3.1.0, priorityqueuejs: ^2.0.0, @commitlint/cli: ^19.2.1, @commitlint/config-conventional: ^19.1.0, @commitlint/prompt-cli: ^19.2.0, @vitest/ui: ^0.33.0, dotenv: ^16.4.5, dotenv-cli: ^7.4.1, happy-dom: ^10.6.3, husky: ^8.0.0, turbo: latest",
+      "auth0/auth0-react: ^2.1.4, hookform/resolvers: ^3.3.4, popperjs/core: ^2.11.8, radix-ui/react-accordion: ^1.1.2, radix-ui/react-alert-dialog: ^1.0.5, radix-ui/react-checkbox: ^1.0.4, radix-ui/react-dialog: ^1.0.5, radix-ui/react-dropdown-menu: ^2.0.6, radix-ui/react-icons: ^1.3.0, radix-ui/react-label: ^2.0.2, radix-ui/react-navigation-menu: ^1.1.4, radix-ui/react-popover: ^1.0.7, radix-ui/react-select: ^2.0.0, radix-ui/react-slot: ^1.0.2, radix-ui/react-switch: ^1.0.3, radix-ui/react-tabs: ^1.0.4, radix-ui/react-toast: ^1.1.5",
+    extraInfo: "",
+  },
+  {
+    name: "Libraries Used",
+    description:
+      "radix-ui/react-toggle: ^1.0.3, radix-ui/react-toggle-group: ^1.0.4, radix-ui/react-tooltip: ^1.0.7, tanstack/react-query: ^5.28.9, tanstack/react-table: ^8.9.3, trpc/client: 11.0.0-next.327, trpc/react-query: 11.0.0-next.327, trpc/server: 11.0.0-next.327, accordion: ^3.0.2, add: ^2.0.6, api: workspace:^, autoprefixer: ^10.4.18, axios: ^1.4.0, class-variance-authority: ^0.7.0, clsx: ^2.1.0, cmdk: 0.2.0, common: workspace:^",
+    extraInfo: "",
+  },
+  {
+    name: "Libraries Used",
+    description:
+      "date-fns: ^3.6.0, dropdown-menu: ^0.1.1, embla-carousel-react: ^8.0.0, framer-motion: ^11.0.28, localforage: ^1.10.0, lucide-react: ^0.363.0, luxon: ^3.4.4, match-sorter: ^6.3.1, next-themes: ^0.3.0, postcss: ^8.4.35, react: ^18.2.0, react-datetime: ^3.2.0, react-day-picker: ^8.10.1, react-dom: ^18.2.0, react-hook-form: ^7.51.3, react-hot-toast: ^2.4.1, react-qr-code: ^2.0.12",
+    extraInfo: "",
+  },
+  {
+    name: "Libraries Used",
+    description:
+      "react-qrcode-logo: ^2.10.0, react-router-bootstrap: ^0.26.2, react-zoom-pan-pinch: ^3.1.0, serve: ^14.2.0, shadcn-ui: ^0.8.0, simplex-noise: ^4.0.1, sonner: ^1.4.41, sort-by: ^1.2.0, superjson: 1.13.3, tailwind-merge: ^2.2.2, tailwindcss: ^3.4.1, tailwindcss-animate: ^1.0.7, toggle-group: ^1.0.5, use-mask-input: ^3.3.7, wouter: ^3.1.0, zod: ^3.23.4, types/luxon: ^3",
+    extraInfo: "",
+  },
+  {
+    name: "Libraries Used",
+    description:
+      "types/node: ^20.2.5, types/prop-types: ^15.7.5, types/react: ^18.0.37, types/react-dom: ^18.0.11, types/react-router-bootstrap: ^0.26.0, vitejs/plugin-react-swc: ^3.0.0, database: workspace:*, eslint: latest, eslint-config-custom: workspace:*, eslint-plugin-react-hooks: ^4.6.0, eslint-plugin-react-refresh: ^0.4.1, prettier: latest, prettier-config-custom: workspace:*, sass: ^1.62.1, tailwindcss-dotted-background: ^1.1.0, tsconfig-custom: workspace:*, typescript: ^5.0.2",
+    extraInfo: "",
+  },
+  {
+    name: "Libraries Used",
+    description:
+      "vite: ^4.3.9, vite-plugin-eslint: ^1.8.1, vitest: ^0.34.1, types/express: ^4.17.21, types/jsonwebtoken: ^9, types/priorityqueuejs: ^1.0.4, xpress: ^4.19.2, jsonwebtoken: ^9.0.2, jwks-rsa: ^3.1.0, priorityqueuejs: ^2.0.0, commitlint/cli: ^19.2.1, commitlint/config-conventional: ^19.1.0, commitlint/prompt-cli: ^19.2.0, vitest/ui: ^0.33.0, dotenv: ^16.4.5, dotenv-cli: ^7.4.1, happy-dom: ^10.6.3, husky: ^8.0.0, turbo: latest",
     extraInfo: "",
   },
 ];
@@ -274,6 +304,12 @@ export default function CreditPage() {
       </div>
       <div className="">
         <InfiniteMovingCardsCreditCommunication />
+      </div>
+      <div className="pt-6 pb-2 text-center">
+        <p>Libraries Used</p>
+      </div>
+      <div className="">
+        <InfiniteMovingCardsCreditLibraries />
       </div>
       <div className="fixed top-0 right-0 mt-4 mr-4 border-2 rounded-lg border-black">
         <Button
