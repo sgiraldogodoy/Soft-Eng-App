738f56ef94e35d7552b96dc93a20def7d6586dd2
Keenan-Porter <116773589+Keenan-Porter@users.noreply.github.com>
2024-04-28 15:19:31 -0400

    feat: created card containing the start page of the User Manual

--- apps/frontend/src/routes/HelpMenu.tsx
+++ apps/frontend/src/routes/HelpMenu.tsx
@@ -1,10 +1,26 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function HelpMenu() {
   return (
     <>
-      <p>
-        I did not hit, I did not hit her. I DID NOT! *throws waterbottle* Oh hi
-        Mark.
-      </p>
+      <Card className="w-auto h-auto m-2 rounded-lg">
+        <CardHeader>
+          <CardTitle className="text-2xl flex justify-center">
+            User Manual
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <CardDescription>hello</CardDescription>
+        </CardContent>
+        <CardFooter>User Manual</CardFooter>
+      </Card>
     </>
   );
 }
