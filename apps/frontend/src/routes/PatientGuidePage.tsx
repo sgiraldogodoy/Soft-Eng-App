1dbe12e9413384e3bd3f98270e4e7230e6d10fce
Keenan <Keedog09@gmail.com>
2024-04-30 02:59:13 -0400

    feat: created and styled the intro to patient features

--- apps/frontend/src/routes/PatientGuidePage.tsx
+++ apps/frontend/src/routes/PatientGuidePage.tsx
@@ -8,6 +8,21 @@ import {
 export default function PatientGuidePage() {
   return (
     <>
+      <p className="font-semibold text-lg text-black col-span-3">
+        Patient Access
+      </p>
+      <br />
+      <p className="text-left w-full col-start-1 col-span-3">
+        &emsp;&emsp;Patients are a vital part of a hospital, without patients,
+        hospitals would not exist. This is a detail that our kiosk refuses to
+        ignore, catering to the very fundamentals of medical care by integrating
+        patient features into the kiosk. All features are added with our users
+        in mind and seek to make the experience of receiving medical care more
+        efficient and less stressful to patients.
+        <br />
+        &emsp;&emsp;However, patients are only allowed to do so much on the
+        kiosk for the very idea of reducing stress in patients.
+      </p>
       <Accordion type="single" collapsible className="w-auto col-span-3 mx-10">
         <AccordionItem value="Intro">
           <AccordionTrigger className="text-black">
