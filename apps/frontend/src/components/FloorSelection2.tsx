8fcf8562de88f8128ffe916ee0fe9817ce052886
ColeGW <68122848+ColeGW@users.noreply.github.com>
2024-04-29 20:49:03 -0400

    fix: floor selector smaller

--- apps/frontend/src/components/FloorSelection2.tsx
+++ apps/frontend/src/components/FloorSelection2.tsx
@@ -7,9 +7,9 @@ interface FloorSelectionProps {
 export default function FloorSelection2({ onFloorClick }: FloorSelectionProps) {
   return (
     <>
-      <div className="bg-white/90 backdrop-blur-md p-4 rounded-[20px]">
+      <div className="bg-white/90 backdrop-blur-md p-3 rounded-[15px]">
         <ToggleGroup
-          size={"lg"}
+          size={"default"}
           type="single"
           orientation="vertical"
           className="flex flex-col"
@@ -19,35 +19,35 @@ export default function FloorSelection2({ onFloorClick }: FloorSelectionProps) {
           <ToggleGroupItem
             value="Floor 3"
             onClick={() => onFloorClick("3", "/03_thethirdfloor.png")}
-            className="w-14 h-14"
+            className="w-10 h-10"
           >
             <span className="text-3xl">3</span>
           </ToggleGroupItem>
           <ToggleGroupItem
             value="Floor 2"
             onClick={() => onFloorClick("2", "/02_thesecondfloor.png")}
-            className="w-14 h-14"
+            className="w-10 h-10"
           >
             <span className="text-3xl">2</span>
           </ToggleGroupItem>
           <ToggleGroupItem
             value="Floor 1"
             onClick={() => onFloorClick("1", "/01_thefirstfloor.png")}
-            className="w-14 h-14"
+            className="w-10 h-10"
           >
             <span className="text-3xl">1</span>
           </ToggleGroupItem>
           <ToggleGroupItem
             value="Floor L1"
             onClick={() => onFloorClick("L1", "/00_thelowerlevel1.png")}
-            className="w-14 h-14"
+            className="w-10 h-10"
           >
             <span className="text-3xl">L1</span>
           </ToggleGroupItem>
           <ToggleGroupItem
             value="Floor L2"
             onClick={() => onFloorClick("L2", "/00_thelowerlevel2.png")}
-            className="w-14 h-14"
+            className="w-10 h-10"
           >
             <span className="text-3xl">L2</span>
           </ToggleGroupItem>
