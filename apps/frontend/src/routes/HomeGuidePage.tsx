10b8b4b9ca7fc94c91051445017f31fabf4156d3
Keenan <Keedog09@gmail.com>
2024-04-30 01:28:09 -0400

    feat: created the pathfinding section with temp photo

--- apps/frontend/src/routes/HomeGuidePage.tsx
+++ apps/frontend/src/routes/HomeGuidePage.tsx
@@ -80,6 +80,108 @@ export default function HomeGuidePage() {
                     return to the Home Page, they can press the arrow on the top
                     right corner of the screen.
                   </p>
+                  <div className="w-full flex justify-center">
+                    <img
+                      src="./helpmenucaptures/Pathfind0.PNG"
+                      alt="homescreen"
+                      className="object-scale-down w-[50%] h-auto rounded-md m-2"
+                    />
+                  </div>
+                  <p>
+                    &emsp;&emsp;Users can use the floor selector on the right to
+                    switch the map between floors by tapping on the floor they
+                    want. Each floor of the hospital is listed and labeled
+                    accordingly, and after selecting the floor, it will be
+                    highlighted reminding Users what floor they are currently
+                    on.
+                  </p>
+                  <div className="w-full flex justify-center">
+                    <img
+                      src="./helpmenucaptures/Pathfind0.PNG"
+                      alt="homescreen"
+                      className="object-scale-down w-[50%] h-auto rounded-md m-2"
+                    />
+                  </div>
+                  <p>
+                    &emsp;&emsp;To add additional context to the symbols used on
+                    the map, hovering over the key button will pull up the Map
+                    Key, listing all pertinent symbols to navigate the hospital.
+                  </p>
+                  <div className="w-full flex justify-center">
+                    <img
+                      src="./helpmenucaptures/Pathfind0.PNG"
+                      alt="homescreen"
+                      className="object-scale-down w-[50%] h-auto rounded-md m-2"
+                    />
+                  </div>
+                  <p>
+                    &emsp;&emsp;Pressing the button with two lines and circles
+                    adjacent to it will allow the user to change additional
+                    settings. They can set their path to prioritize ease of
+                    mobility, helping those in wheelchairs and other mobility
+                    impairments, they can select the algorithm for their
+                    preferred style of navigation (the algorithms include A*,
+                    Dijkstra, Breadth first search, and depth first search with
+                    A* being the default) , and they can select their default
+                    start location.
+                  </p>
+                  <div className="w-full flex justify-center">
+                    <img
+                      src="./helpmenucaptures/Pathfind0.PNG"
+                      alt="homescreen"
+                      className="object-scale-down w-[50%] h-auto rounded-md m-2"
+                    />
+                  </div>
+                  <p>
+                    &emsp;&emsp;Users can select the search bars on the top of
+                    the screen to input a starting location and destination.
+                    They can peruse the dropdown for their required destination,
+                    or they can search for a location directly, which will
+                    filter out irrelevant locations.
+                  </p>
+                  <div className="w-full flex justify-center">
+                    <img
+                      src="./helpmenucaptures/Pathfind0.PNG"
+                      alt="homescreen"
+                      className="object-scale-down w-[50%] h-auto rounded-md m-2"
+                    />
+                  </div>
+                  <p>
+                    &emsp;&emsp;Users can also alternatively click nodes on the
+                    map to set locations. Once two nodes have been set, or both
+                    search bars have been filled, directions will be displayed
+                    with an animated line traveling toward the final
+                    destination. Additionally, text directions will appear
+                    giving clear and concise directions. These directions can
+                    also be heard, by clicking the play button users can be read
+                    allowed directions through text to speech. These text
+                    directions can also be taken along with users by scanning
+                    the QR code displayed at the bottom of the screen with their
+                    phone, which will bring the users to a phone friendly
+                    website with the text directions displayed.
+                  </p>
+                  <div className="w-full flex justify-center">
+                    <img
+                      src="./helpmenucaptures/Pathfind0.PNG"
+                      alt="homescreen"
+                      className="object-scale-down w-[50%] h-auto rounded-md m-2"
+                    />
+                  </div>
+                  <p>
+                    While any user can access pathfinding without logging in,
+                    logged in users are also able to access the page. Logged in
+                    users will automatically be directed to the same page, but
+                    will gain the additional feature of a navigation bar which
+                    grants them access to features that are otherwise
+                    restricted.
+                  </p>
+                  <div className="w-full flex justify-center">
+                    <img
+                      src="./helpmenucaptures/Pathfind0.PNG"
+                      alt="homescreen"
+                      className="object-scale-down w-[50%] h-auto rounded-md m-2"
+                    />
+                  </div>
                 </AccordionContent>
               </AccordionItem>
             </Accordion>
